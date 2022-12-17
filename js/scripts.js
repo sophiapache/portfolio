3f3a697edc2a8d6a58b01b7d87408834b3f48009
Sophia Pache <sophpache@gmail.com>
2022-12-17 11:30:38 +1100

    linked js correctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ const navlist = $(".nav-list");
 const links = $(".nav-list li");
 
 $(".hamb").click(() => {
-  console.log("hello");
   $("#menu").toggleClass("nav-list");
   // $(".nav-list").css("visibility", "visible");
 });
